19ab6b70c78abd802040b36559f15c7dcc707871
Lucien Faure <faurelucien@proton.me>
2025-05-20 16:55:45 +0200

    removed header

---
+++ dev_no_re.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+scene=$1
+
+RED='\033[0;31m'
+RESET='\033[0m'
+
+if [ $# == 0 ]; then
+	printf "${RED}usage ./dev.sh <map_number>\n${RESET}"
+	exit 1
+fi
+
+# make fclean
+# make re
+make -s
+printf "\n\n"
+if [ $scene == 1 ]; then
+	./miniRT assets/scenes/subject_example.rt
+elif [ $scene == 2 ]; then
+	./miniRT assets/scenes/subject_disorder.rt
+elif [ $scene == 3 ]; then
+	./miniRT assets/scenes/one_sphere.rt
+elif [ $scene == 4 ]; then
+	./miniRT assets/scenes/several_cylinders.rt
+elif [ $scene == 5 ]; then
+	echo scene slot 5 is empty
+elif [ $scene == 6 ]; then
+	echo scene slot 6 is empty
+elif [ $scene == 7 ]; then
+	echo scene slot 7 is empty
+elif [ $scene == 8 ]; then
+	echo scene slot 8 is empty
+fi
