0723cb547172e10581ce19652ef81251bfaa7ab0
avdplassche <alexandre.vanderp@gmail.com>
2025-05-21 22:40:43 +0200

    last changes

--- src/main.c
+++ src/main.c
@@ -1,33 +1,14 @@
 #include "minirt.h"
 
-void	init_struct(t_mlx_data *mlx, t_img_data *img, t_scene *scene, t_minirt *minirt, t_render *render)
+void	initialize_scene_ptr(t_scene *scene)
 {
-	ft_memset(minirt, '\0', sizeof(t_minirt));
-	ft_memset(mlx, '\0', sizeof(t_mlx_data));
-	ft_memset(img, '\0', sizeof(t_img_data));
-	ft_memset(scene, '\0', sizeof(t_scene));
-	ft_memset(render, '\0', sizeof(t_render));
-
-	minirt->mlx = mlx;
-	minirt->mlx->img_st = img;
-	minirt->scene = scene;
-	minirt->render = render;
-}
-
-int	start_render(t_minirt *minirt)
-{
-	clock_t	t;
-	double	time_taken;
-	int		err;
-
-	t = clock();
-
-	err = render_scene(minirt, minirt->mlx, minirt->scene);
-
-	t = clock() - t;
-	time_taken = ((double)t) / CLOCKS_PER_SEC;
-	printf("rendered image in %f seconds\n", time_taken);
-	return (err);
+	scene->ambient = NULL;
+	scene->camera = NULL;
+	scene->light = NULL;
+	scene->planes = NULL;
+	scene->spheres = NULL;
+	scene->cylinders = NULL;
+	scene->buffer = NULL;
 }
 
 int	main(int ac, char **av)
@@ -36,21 +17,37 @@ int	main(int ac, char **av)
 	t_img_data	img;
 	t_scene		scene;
 	t_minirt	minirt;
-	t_render	render;
 
-	init_struct(&mlx, &img, &scene, &minirt, &render);
+	ft_memset(&mlx, '\0', sizeof(t_mlx_data));
+	ft_memset(&img, '\0', sizeof(t_img_data));
+	ft_memset(&scene, '\0', sizeof(t_scene));
+	ft_memset(&minirt, '\0', sizeof(t_minirt));
+	// initialize_scene_ptr(&scene);
+	minirt.mlx = &mlx;
+	minirt.scene = &scene;
+	minirt.mlx->img_st = &img;
+
 	if (ac != 2)
 		return (ft_printf("Usage: <scene.rt>\n"), 1);
+
 	parse_scene(&minirt, av[1]);
+
 	if (WIN_H < 10 || WIN_W < 10)
 		quit(&minirt, WIN_SIZE_ERR);
 	if (init_structure(&mlx, &img))
 		quit(&minirt, MLX_ERR);
 	init_events(&mlx);
 
-	start_render(&minirt);
+	clock_t t;
+	t = clock();
 
+	render_scene(&minirt, &mlx, &scene);
+
+	t = clock() - t;
+	double time_taken = ((double)t) / CLOCKS_PER_SEC;
+	printf("rendered image in %f seconds\n", time_taken);
 	mlx_loop(mlx.mlx);
+	CLOSE_DEBUG_FD;
 	quit(&minirt, "\033[32mQuiting program\n");
 	return (0);
 }
