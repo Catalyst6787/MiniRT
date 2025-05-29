24b189da953e761f2c61c3a374b6354b6dbaa774
Lucien Faure <faurelucien@proton.me>
2025-05-29 13:21:51 +0200

    removed vec3_normalise_inplace()

--- includes/vec3.h
+++ includes/vec3.h
@@ -33,12 +33,6 @@ int		copy_vec3(t_vec3 *dest, t_vec3 *origin);
 
 
 t_vec3	vec3_reverse(t_vec3 vec);
-// int		vec3_reverse_inplace(t_vec3 *vec_to_rev);
-// int		vec3_substract_inplace(t_vec3 *vec_value, const t_vec3 *to_subtract);
-// int		vec3_add_inplace(t_vec3 *vec_value, const t_vec3 *to_add);
-// int		vec3_multiply_inplace(t_vec3 *vec_value, const t_vec3 *to_multiply_by);
-// int		vec3_divide_inplace(t_vec3 *vec_value, const t_vec3 *to_divide_by);
-
 int		set_vec3(t_vec3 *v, double x, double y, double z);
 int		copy_vec3(t_vec3 *dest, t_vec3 *src);
 t_vec3	get_vec3(double x, double y, double z);
@@ -62,7 +56,6 @@ t_vec3	vec3_double_division(t_vec3 src, double n);
 
 double	vec3_length_squared(t_vec3 *vec3);
 double	vec3_exact_length(t_vec3 *vec3);
-void	vec3_normalise_inplace(t_vec3 *vec3);
 t_vec3	vec3_normalise(t_vec3 vec3);
 double	vec3_dot(t_vec3 *vec3_u, t_vec3 *vec3_v);
 int		vec3_isequal(t_vec3 *vec3_a, t_vec3	*vec3_b);
