3bf642a0f311d2d2bd78a4cd7ff06a8064e7b9fc
Lucien Faure <faurelucien@proton.me>
2025-06-18 13:52:26 +0200

    WIP lighting tests

--- src/tests/test_lighting.c
+++ src/tests/test_lighting.c
@@ -1,9 +1,20 @@
 #include "minirt.h"
 
-t_vec3	get_lighting(t_lighting	l)
+t_vec3	get_lighting(t_lighting	lighting)
 {
-	(void)l;
-	return (get_vec3(0, 0, 0));
+	t_vec3	effective_color;
+	t_vec3	light_vector;
+	t_vec3	ambient;
+	t_vec3	diffuse;
+	t_vec3	reflectv;
+	double	light_dot_normal;
+	double	reflect_dot_eye;
+
+	effective_color = vec3_vec_multiplication(lighting.m.color, lighting.light.color);
+	effective_color = vec3_double_multiplication(effective_color, lighting.light.brightness);
+	light_vector = vec3_normalise(vec3_vec_substraction(lighting.light.pos, lighting.pos));
+	ambient = vec3_double_multiplication(effective_color, lighting.m.ambient);
+
 }
 
 t_light	get_light(t_vec3 pos, double brightness, t_vec3 color)
@@ -30,6 +41,7 @@ int	start_all_lighting_tests(void)
 	l.light = get_light(get_point3(0, 0, -10), 1, get_vec3(1, 1, 1));
 	result = get_lighting(l);
 	assert(vec3_isequal(result, get_color(1.9, 1.9, 1.9)));
+	printf("Lighting with the eye between the light and the surface passed\n");
 
 	// Lighting with the eye between light and surface, eye offset 45°
 	l.eyev = get_vec3(0, sqrt(2)/2, sqrt(2)/2);
@@ -37,6 +49,7 @@ int	start_all_lighting_tests(void)
 	l.light = get_light(get_point3(0, 0, -10), 1, get_vec3(1, 1, 1));
 	result = get_lighting(l);
 	assert(vec3_isequal(result, get_color(1.0, 1.0, 1.0)));
+	printf("Lighting with the eye between light and surface, eye offset 45° passed\n");
 
 	// Lighting with eye opposite surface, light offset 45°
 	l.eyev = get_vec3(0, 0, -1);
@@ -44,11 +57,22 @@ int	start_all_lighting_tests(void)
 	l.light = get_light(get_point3(0, 10, -10), 1, get_color(1, 1, 1));
 	result = get_lighting(l);
 	assert(vec3_isequal(result, get_color(0.7364, 0.7364, 0.7364)));
+	printf("Lighting with eye opposite surface, light offset 45° passed\n");
+
 	// Lighting with eye in the path of the reflection vector
 	l.eyev = get_vec3(0, -sqrt(2)/2, -sqrt(2)/2);
 	l.normalv = get_vec3(0, 0, -1);
 	l.light = get_light(get_point3(0, 10, -10), 1, get_color(1, 1, 1));
 	result = get_lighting(l);
 	assert(vec3_isequal((result), get_color(1.6364, 1.6364, 1.6364)));
+	printf("Lighting with eye in the path of the reflection vector passed\n");
+
+	// Lighting with the light behind the surface
+	l.eyev = get_vec3(0, 0, -1);
+	l.normalv = get_vec3(0, 0, -1);
+	l.light = get_light(get_point3(0, 0, 10), 1, get_color(1, 1, 1));
+	result = get_lighting(l);
+	assert(vec3_isequal(result, get_color(0.1, 0.1, 0.1)));
+	printf("Lighting with the light behind the surface passed\n");
 	return (0);
 }
