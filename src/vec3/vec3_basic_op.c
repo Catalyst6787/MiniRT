718df62e459b30e60c5d70582ee7b80137093d60
Lucien Faure <faurelucien@proton.me>
2025-06-18 17:16:58 +0200

    fixed a few undefined values

--- src/vec3/vec3_basic_op.c
+++ src/vec3/vec3_basic_op.c
@@ -98,7 +98,7 @@ t_vec3	vec3_vec_multiplication(t_vec3 v1, t_vec3 v2)
 {
 	t_vec3	v;
 
-	if (v1.w || v1.w)
+	if (v1.w || v2.w)
 		print_err(FILE, LINE, "vec3_vec_multiplication, trying to multiply a point");
 	v.x = v1.x * v2.x;
 	v.y = v1.y * v2.y;
