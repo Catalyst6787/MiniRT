63594695d16fc902a43d3bcb1ff0bc8d59f7a763
Alexandre Van der Plassche <vanderplassche.alexandre@gmail.com>
2025-06-18 15:09:01 +0200

    changed color parsing

---
+++ src/tests/tests_world.c
@@ -0,0 +1,27 @@
+#include "minirt.h"
+
+
+// t_light	*new_light(t_vec3 pos, t_vec3 color)
+// {
+// 	t_light	*light;
+	
+// 	light = malloc(sizeof(t_light));
+// 	if (!light)
+// 		return(NULL);
+// 	light->pos = get_point3(pos.x, pos.y, pos.z);
+// 	light->color = get_color(color.x, color.y, color.z);
+// 	return (light);
+// }
+
+
+// void	test_world()
+// {
+// 	t_light		*light;
+// 	t_sphere	*s1;
+// 	t_sphere	*s2;
+
+// 	light = new_light(get_point3(-10, 10, -10), get_color(1, 1, 1));
+// 	// s1 = new_sphere(s)
+
+// }
+
