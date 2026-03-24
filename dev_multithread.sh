bbfbb47965b652145317e62d2b1ff8c299228019
avdplassche <vanderplassche.alexandre@gmail.com>
2026-03-24 16:56:19 +0100

    Delete dev_multithread.sh

--- dev_multithread.sh
+++
@@ -1,8 +0,0 @@
-make bonus
-valgrind --tool=helgrind --log-file=./log/helgrind.log ./miniRT_bonus scenes/subject_example.rt
-# valgrind --tool=drd --log-file=./log/helgrind.log -s ./miniRT_bonus scenes/subject_example.rt
-# valgrind --leak-check=full --show-leak-kinds=all --log-file=./log/valgrind.log ./miniRT_bonus scenes/subject_example.rt
-# valgrind --leak-check=full --log-file=./log/valgrind.log ./miniRT scenes/several_cylinders.rt
-# valgrind --leak-check=full --log-file=./log/valgrind.log ./miniRT scenes/one_sphere.rt
-# valgrind --leak-check=full --log-file=./log/valgrind.log ./miniRT scenes/invalid/empty.rt
-# valgrind --leak-check=full --log-file=./log/valgrind.log ./miniRT  scenes/invalid/invalid_char.rt
