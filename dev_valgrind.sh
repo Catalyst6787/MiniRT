9445c623f21c004bfd95ba444f73c3d2ddbbaf46
avdplassche <vanderplassche.alexandre@gmail.com>
2026-03-24 16:56:50 +0100

    Delete dev_valgrind.sh

--- dev_valgrind.sh
+++
@@ -1,6 +0,0 @@
-make bonus
-valgrind --leak-check=full --show-leak-kinds=all --log-file=./log/valgrind.log ./miniRT_bonus scenes/subject_example.rt
-# valgrind --leak-check=full --log-file=./log/valgrind.log ./miniRT scenes/several_cylinders.rt
-# valgrind --leak-check=full --log-file=./log/valgrind.log ./miniRT scenes/one_sphere.rt
-# valgrind --leak-check=full --log-file=./log/valgrind.log ./miniRT scenes/invalid/empty.rt
-# valgrind --leak-check=full --log-file=./log/valgrind.log ./miniRT  scenes/invalid/invalid_char.rt
